4d734b28372fb0c4b591905c1ac7b41e4f5ccd0e
Pranaav B V <28bvpranaav@gmail.com>
2026-02-09 22:19:57 +0530

    Refactor singleNumber method to improve logic and remove unnecessary HashSet usage

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,20 +1,24 @@
-import java.util.HashSet;
-
 public class SingleNumber {
-    public int singleNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int val : nums) {
-            set.add(val);
-        }
-
-        for (int ans : set) {
-            return ans;
+    public static int singleNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (j == nums.length - 1 && i == nums.length - 1) {
+                    return nums[i];
+                } else if (j == i) {
+                    continue;
+                } else if (nums[i] == nums[j]) {
+                    break;
+                } else if (j == nums.length - 1 && nums[i] != nums[j]) {
+                    return nums[i];
+                }
+            }
         }
 
         return -1;
     }
 
     public static void main(String[] args) {
-
+        int[] nums = { 2, 2, 1 };
+        System.out.println(singleNumber(nums));
     }
 }
